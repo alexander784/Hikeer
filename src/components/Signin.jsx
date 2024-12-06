bef6bbd0b94c923b97e954295b3c69657fb013a2
alexander784 <alexandernyaga1999@gmail.com>
2024-12-06 11:46:57 +0300

    Login Logic

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,35 +1,55 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import { useAuth } from '../Context/AuthContext';
 
 
 const Signin = () => {
+  const {login} = useAuth();
+
+  const handleGoogleSuccess = () => {
+    const user = credentialResponse?.credential;
+    if(user) {
+      login(user);
+      console.log('Logged in with Google', user);
+    }
+  };
+
+  const handleEmailSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+
+    if (email && password) {
+      const name = email.split('@')[0]
+      login(name);
+      console.log('logged in with email', name);
+      
+    }
+  }
   return (
     <div className='flex justify-center items-center min-h-screen bg-gray-100'>
       <div className='bg-white p-8 rounded-lg shadow-lg w-96'>
       <h1 className='text-center text-xl'>Login</h1>
-      <form>
-      <input type='email' placeholder='Email' className='w-full p-2 border border-gray-300 rounded'
-      />
-      <input type='password' placeholder='Password' className='w-full p-2 mb-4 border border-gray-300 rounded'
-      />
-      <button type='submit' className='w-full py-2 bg-zinc-600 text-white rounded'>
-        Sign In
-      </button>
-    </form>
-    <h1 className='text-center text-black'>Login with Google</h1>
+        <form>
+            <input type='email' placeholder='Email' className='w-full p-2 border border-gray-300 rounded'
+            />
+            <input type='password' placeholder='Password' className='w-full p-2 mb-4 border border-gray-300 rounded'
+            />
+          <button type='submit' className='w-full py-2 bg-zinc-600 text-white rounded'>
+            Sign In
+          </button>
+        </form>
+       <h1 className='text-center text-black'>Login with Google</h1>
 
-    <GoogleLogin
-       onSuccess={credentialResponse => {
-        console.log(credentialResponse);
-    }}
-    onError={() => {
-        console.log('Login Failed');
-    }}
-    />
-    <div className='mt-4 text-center text-sm text-black'>
-    </div>
-    
-
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+            }}
+            onError={() => {
+                console.log('Login Failed');
+            }}
+          />
     </div>
     </div>
 
