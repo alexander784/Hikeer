d39bd705dd507f1f539415aab76aacbb0d86d536
alexander784 <alexandernyaga1999@gmail.com>
2024-12-06 11:39:56 +0300

    Structure Auth context

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Children, createContext, useContext, useState } from 'react'
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
+const AuthContext = createContext();
+
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+
+};
+
+export const AuthProvider = () => {
+    const[username, setUserName] = useState('');
+
+    const login = () => {
+        setUserName(name);
+
+    };
+    const logout = () => {
+        setUserName('');
+    };
 }
+  return (
+    <AuthContext.Provider value={{ userName, login, logout}}>
+        {Children}
+    </AuthContext.Provider>
+  )
 
 export default AuthContext;
