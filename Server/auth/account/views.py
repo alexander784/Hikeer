34c7c14507ad0a85388ef23a0d07e22c13f910c9
alexander784 <alexandernyaga1999@gmail.com>
2025-01-31 06:26:02 +0300

    Login and get user

--- Server/auth/account/views.py
+++ Server/auth/account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from serializer import RegisterSerializer
+from serializer import RegisterSerializer,LoginSerializer,UserSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import get_user_model,authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create your views here.
@@ -19,4 +21,29 @@ def register_user(request):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def login_user(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        email = serializer.validated_data['email']
+        password = serializer.validates_data['password']
+        user = authenticate(email=email, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh':str(refresh),
+                'access':str(refresh.access_token),
+                'user':UserSerializer(user).data
+            }, status=status.HTTP_200_SUCCESS)
+        
+    return Response({"error":"Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_details(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
 
